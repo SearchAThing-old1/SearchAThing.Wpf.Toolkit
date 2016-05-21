9165504cf505508e713fae8da63281572ca5778c
Lorenzo Delana <oss.devel@searchathing.com>
2016-05-21 20:07:25 +0200

    status manager

---
+++ src/SearchAThing.Wpf.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SearchAThing.Wpf.Toolkit")]
+[assembly: AssemblyDescription("Toolkit for the WPF framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SearchAThing.Wpf.Toolkit")]
+[assembly: AssemblyCopyright("Copyright © Lorenzo Delana 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a413694a-6ee9-4c92-ab7a-c7ee8c92a2ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
---
+++ src/SearchAThing.Wpf.Toolkit/StatusManager.cs
@@ -0,0 +1,110 @@
+﻿#region SearchAThing.Wpf.Toolkit, Copyright(C) 2016 Lorenzo Delana, License under MIT
+/*
+* The MIT License(MIT)
+* Copyright(c) 2016 Lorenzo Delana, https://searchathing.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SearchAThing.Wpf.Toolkit
+{
+
+    /// <summary>
+    /// Manage concurrent status set, with detect of the last status release.
+    /// </summary>
+    public class StatusManager : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        uint statusId;
+        object statusIdLck;
+
+        HashSet<uint> statusIdSet;
+
+        public StatusManager()
+        {
+            statusId = 0;
+            statusIdSet = new HashSet<uint>();
+            statusIdLck = new object();
+        }
+
+        string _status;
+        /// <summary>
+        /// Bind your textblock to this property.
+        /// You can change the status simply by changing this property value.
+        /// </summary>
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Status"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// For long-running task in order to release the status
+        /// displaying some ready message, use the NewStatus as first
+        /// then release at the end with the id this function return.
+        /// </summary>        
+        public uint NewStatus(string msg)
+        {
+            var id = 0u;
+
+            lock (statusIdLck)
+            {
+                id = ++statusId;
+                statusIdSet.Add(statusId);
+            }
+
+            Status = msg;
+
+            return id;
+        }        
+
+        /// <summary>
+        /// Set the given msg ready status if no other status are actually running.
+        /// </summary>        
+        public void ReleaseStatus(uint id, string msg = "Ready.")
+        {
+            var empty = false;
+
+            lock (statusIdLck)
+            {
+                statusIdSet.Remove(id);
+                empty = statusIdSet.Count == 0;
+            }
+            
+            if (empty) Status = msg;
+        }
+
+    }
+
+}
